07c2f319cccdd3b0b7213421a06bc9bdbf4ae979
Shawn <shawntennity@gmail.com>
2019-11-30 10:48:35 -0500

    Added serializer gem to gemfile in anticipation of it being needed.  Fixed issue with helpers in application controller.  Changed default local port.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  # mock logged in user
+    def current_user
+      User.first
+    end
+  
+    def logged_in?
+      !!current_user
+    end
 end
