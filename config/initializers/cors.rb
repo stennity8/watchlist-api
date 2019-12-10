7c246d435e68f22530799e5e165f795177af0029
Shawn <shawntennity@gmail.com>
2019-12-10 17:25:09 -0500

    Set CORS for Netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,8 +6,17 @@
 # Read more: https://github.com/cyu/rack-cors
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  # allow do
+  #   origins 'http://localhost:3000'
+
+  #   resource '*',
+  #     headers: :any,
+  #     methods: [:get, :post, :put, :patch, :delete, :options, :head],
+  #     credentials: true
+  # end
+
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://watch-list.netlify.com'
 
     resource '*',
       headers: :any,
